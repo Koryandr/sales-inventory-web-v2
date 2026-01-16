881fa2fa79773f1bac4118a028749ebac06308ae
Kory <kory.tashim@icloud.com>
2026-01-16 11:46:33 +0500

    Работа над структурой

--- src/components/Analytics.tsx
+++ src/components/Analytics.tsx
@@ -1,7 +1,15 @@
+import "./Analytics.css"
+
 const Analytics = () => {
     return(
         <>
-        
+        <div className="px-20 py-10">
+            <h1 className="analyze-text">Аналитика</h1>
+            <div className="Total-amount">
+                <h2>Общая прибыль</h2>
+                <p>+ 287 500 KZT</p>
+            </div>
+        </div>
         </>
     )
 }
--- src/components/Sales.tsx
+++ src/components/Sales.tsx
@@ -1,7 +1,37 @@
+import "./Sales.css"
+
 const Sales = () => {
     return(
         <>
-        
+        <div className="flex gap-10 px-20 py-10">
+            <h1 className="main-text">Продажи</h1>
+            <button className="add-sales">+ Добавить продажу</button>
+        </div>
+
+        <div className="px-25">
+            <table className="border border-gray-400 border-collapse">
+                <thead>
+                    <tr>
+                    <th className="border border-gray-400 p-2">Товар</th>
+                    <th className="border border-gray-400 p-2">Сумма</th>
+                    <th className="border border-gray-400 p-2">Дата</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                    <td className="border border-gray-400 p-2">Смартфоны</td>
+                    <td className="border border-gray-400 p-2">20 000</td>
+                    <td className="border border-gray-400 p-2">20.12.2025</td>
+                    </tr>
+                    <tr>
+                    <td className="border border-gray-400 p-2">Чайники</td>
+                    <td className="border border-gray-400 p-2">3 500</td>
+                    <td className="border border-gray-400 p-2">20.12.2025</td>
+                    </tr>
+                    
+                </tbody>
+            </table>
+        </div>
         </>
     )
 }
